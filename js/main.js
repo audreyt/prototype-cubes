9d19d806c21ab00c770206638f4ac2b1bfdb0aab
caasi Huang <caasi.igd@gmail.com>
2014-07-18 21:52:00 +0800

    use bower directly, try a simple react.js example

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 (function(){
-  console.log('foobar');
+  var Timer;
+  Timer = React.createClass({
+    displayName: 'Timer',
+    getInitialState: function(){
+      return {
+        secondsElapsed: 0
+      };
+    },
+    tick: function(){
+      this.setState({
+        secondsElapsed: this.state.secondsElapsed + 1
+      });
+    },
+    componentDidMount: function(){
+      this.interval = setInterval(this.tick, 1000);
+    },
+    componentWillUnmount: function(){
+      clearInterval(this.interval);
+    },
+    render: function(){
+      return React.DOM.div(null, 'Seconds Elapsed: ', this.state.secondsElapsed);
+    }
+  });
+  React.renderComponent(Timer(), document.getElementById('container'));
 }).call(this);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -1,1 +1,15 @@
-console.log \foobar
+Timer = React.createClass do
+  displayName: \Timer
+  getInitialState: ->
+    secondsElapsed: 0
+  tick: !->
+    @setState secondsElapsed: @state.secondsElapsed + 1
+  componentDidMount: !->
+    @interval = setInterval @tick, 1000
+  componentWillUnmount: !->
+    clearInterval @interval
+  render: ->
+    React.DOM.div null, 'Seconds Elapsed: ', @state.secondsElapsed
+
+React.renderComponent Timer!, document.getElementById \container
+
