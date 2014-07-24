87b4ff91f349015c78a2f2189b4a80fdca8ed2bb
caasi Huang <caasi.igd@gmail.com>
2014-07-24 20:33:45 +0800

    migrate to react 0.11.0, practice more

--- js/main.js
+++ js/main.js
@@ -1,26 +1,69 @@
 (function(){
-  var Timer;
-  Timer = React.createClass({
-    displayName: 'Timer',
-    getInitialState: function(){
-      return {
-        secondsElapsed: 0
-      };
+  var ref$, div, a, CC;
+  ref$ = React.DOM, div = ref$.div, a = ref$.a;
+  CC == null && (CC = {
+    config: {
+      width: 200,
+      height: 200
     },
-    tick: function(){
-      this.setState({
-        secondsElapsed: this.state.secondsElapsed + 1
-      });
-    },
-    componentDidMount: function(){
-      this.interval = setInterval(this.tick, 1000);
-    },
-    componentWillUnmount: function(){
-      clearInterval(this.interval);
-    },
-    render: function(){
-      return React.DOM.div(null, 'Seconds Elapsed: ', this.state.secondsElapsed);
-    }
+    List: React.createClass({
+      displayName: 'List',
+      getInitialState: function(){
+        return {
+          x: 200,
+          y: 200,
+          width: CC.config.width,
+          height: CC.config.height,
+          words: [0]
+        };
+      },
+      leftClicked: function(e){
+        e.preventDefault();
+        this.state.words.unshift(0);
+        this.setState({
+          x: this.state.x - CC.config.width,
+          width: this.state.width + CC.config.width,
+          words: this.state.words
+        });
+        return console.log(this.state.words);
+      },
+      rightClicked: function(e){
+        e.preventDefault();
+        this.state.words.push(0);
+        this.setState({
+          width: this.state.width + CC.config.width,
+          words: this.state.words
+        });
+        return console.log(this.state.words);
+      },
+      render: function(){
+        return div({
+          className: 'cube-list',
+          style: {
+            left: this.state.x,
+            top: this.state.y,
+            width: this.state.width,
+            height: this.state.height
+          }
+        }, a({
+          className: 'hit left',
+          style: {
+            width: CC.config.width / 2,
+            height: CC.config.height
+          },
+          href: '#',
+          onClick: this.leftClicked
+        }), a({
+          className: 'hit right',
+          style: {
+            width: CC.config.width / 2,
+            height: CC.config.height
+          },
+          href: '#',
+          onClick: this.rightClicked
+        }));
+      }
+    })
   });
-  React.renderComponent(Timer(), document.getElementById('container'));
+  React.renderComponent(CC.List(), document.getElementById('container'));
 }).call(this);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -1,15 +1,54 @@
-Timer = React.createClass do
-  displayName: \Timer
-  getInitialState: ->
-    secondsElapsed: 0
-  tick: !->
-    @setState secondsElapsed: @state.secondsElapsed + 1
-  componentDidMount: !->
-    @interval = setInterval @tick, 1000
-  componentWillUnmount: !->
-    clearInterval @interval
-  render: ->
-    React.DOM.div null, 'Seconds Elapsed: ', @state.secondsElapsed
+{div, a} = React.DOM
 
-React.renderComponent Timer!, document.getElementById \container
+CC ?= do
+  config:
+    width:  200
+    height: 200
+  List: React.createClass do
+    displayName: \List
+    getInitialState: ->
+      x: 200
+      y: 200
+      width:  CC.config.width
+      height: CC.config.height
+      words: [0]
+    leftClicked: (e) ->
+      e.preventDefault!
+      @state.words.unshift 0
+      @setState do
+        x: @state.x - CC.config.width
+        width: @state.width + CC.config.width
+        words: @state.words
+      console.log @state.words
+    rightClicked: (e) ->
+      e.preventDefault!
+      @state.words.push 0
+      @setState do
+        width: @state.width + CC.config.width
+        words: @state.words
+      console.log @state.words
+    render: ->
+      div do
+        className: 'cube-list'
+        style:
+          left:   @state.x
+          top:    @state.y
+          width:  @state.width
+          height: @state.height
+        * a do
+            className: 'hit left'
+            style:
+              width:  CC.config.width / 2
+              height: CC.config.height
+            href: '#'
+            onClick: @leftClicked
+        * a do
+            className: 'hit right'
+            style:
+              width:  CC.config.width / 2
+              height: CC.config.height
+            href: '#'
+            onClick: @rightClicked
+
+React.renderComponent CC.List!, document.getElementById \container
 
