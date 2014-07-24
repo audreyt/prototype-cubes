4f19f88c8e7cac1e95c220084ebec43bdd6b9fdd
caasi Huang <caasi.igd@gmail.com>
2014-07-25 00:20:31 +0800

    play with mixins

--- js/main.js
+++ js/main.js
@@ -1,19 +1,71 @@
 (function(){
-  var ref$, div, a, CC;
+  var ref$, div, a, config, Draggable, Hittable, CC;
   ref$ = React.DOM, div = ref$.div, a = ref$.a;
-  CC == null && (CC = {
-    config: {
-      width: 200,
-      height: 200
+  config = {
+    root: document.getElementById('container'),
+    width: 200,
+    height: 200
+  };
+  Draggable = {
+    mixin: {
+      componentDidMount: function(){
+        $(this.getDOMNode()).on('mousedown', this.onMouseDown);
+      },
+      componentWillUnmount: function(){
+        $(this.getDOMNode()).off('mousedown', this.onMouseDown);
+      },
+      onMouseDown: function(e){
+        this.comp = {
+          x: this.state.x,
+          y: this.state.y
+        };
+        this.mouse = {
+          x: e.pageX,
+          y: e.pageY
+        };
+        $(window).on('mousemove', this.onMouseMove).on('mouseup', this.onMouseUp);
+        $(this.getDOMNode()).off('mousedown', this.onMouseDown);
+      },
+      onMouseUp: function(e){
+        $(window).off('mousemove', this.onMouseMove).off('mouseup', this.onMouseUp);
+        $(this.getDOMNode()).on('mousedown', this.onMouseDown);
+      },
+      onMouseMove: function(e){
+        this.setState({
+          x: this.comp.x + e.pageX - this.mouse.x,
+          y: this.comp.y + e.pageY - this.mouse.y
+        });
+      }
+    }
+  };
+  Hittable = {
+    list: [],
+    status: {
+      OUT: 0x00,
+      OVERLAP: 0x01,
+      IN: 0x03
     },
+    mixin: {
+      componentDidMount: function(){
+        Hittable.list.push(this);
+      },
+      componentWillUnmount: function(){
+        Hittable.list = Hittable.list.filter((function(it){
+          return it !== this;
+        }), this);
+      }
+    }
+  };
+  CC == null && (CC = {
     List: React.createClass({
       displayName: 'List',
+      mixins: [Draggable.mixin, Hittable.mixin],
       getInitialState: function(){
         return {
           x: 200,
           y: 200,
-          width: CC.config.width,
-          height: CC.config.height,
+          width: config.width,
+          height: config.height,
           words: [0]
         };
       },
@@ -21,8 +73,8 @@
         e.preventDefault();
         this.state.words.unshift(0);
         this.setState({
-          x: this.state.x - CC.config.width,
-          width: this.state.width + CC.config.width,
+          x: this.state.x - config.width,
+          width: this.state.width + config.width,
           words: this.state.words
         });
         return console.log(this.state.words);
@@ -31,7 +83,7 @@
         e.preventDefault();
         this.state.words.push(0);
         this.setState({
-          width: this.state.width + CC.config.width,
+          width: this.state.width + config.width,
           words: this.state.words
         });
         return console.log(this.state.words);
@@ -45,25 +97,25 @@
             width: this.state.width,
             height: this.state.height
           }
-        }, a({
-          className: 'hit left',
-          style: {
-            width: CC.config.width / 2,
-            height: CC.config.height
-          },
-          href: '#',
-          onClick: this.leftClicked
-        }), a({
-          className: 'hit right',
-          style: {
-            width: CC.config.width / 2,
-            height: CC.config.height
-          },
-          href: '#',
-          onClick: this.rightClicked
-        }));
+          /*
+          * a do
+              className: 'hit left'
+              style:
+                width:  config.width / 2
+                height: config.height
+              href: '#'
+              #onClick: @leftClicked
+          * a do
+              className: 'hit right'
+              style:
+                width:  config.width / 2
+                height: config.height
+              href: '#'
+              #onClick: @rightClicked
+          */
+        });
       }
     })
   });
-  React.renderComponent(CC.List(), document.getElementById('container'));
+  React.renderComponent(CC.List(), config.root);
 }).call(this);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -1,30 +1,76 @@
 {div, a} = React.DOM
 
+config = do
+  root: document.getElementById \container
+  width:  200
+  height: 200
+
+Draggable = do
+  mixin:
+    componentDidMount: !->
+      $ @getDOMNode! .on \mousedown @onMouseDown
+    componentWillUnmount: !->
+      $ @getDOMNode! .off \mousedown @onMouseDown
+    onMouseDown: !(e) ->
+      @comp = do
+        x: @state.x
+        y: @state.y
+      @mouse = do
+        x: e.pageX
+        y: e.pageY
+      # register listeners at topmost element
+      $ window
+        .on \mousemove @onMouseMove
+        .on \mouseup   @onMouseUp
+      $ @getDOMNode! .off \mousedown @onMouseDown
+    onMouseUp: !(e) ->
+      $ window
+        .off \mousemove @onMouseMove
+        .off \mouseup   @onMouseUp
+      $ @getDOMNode! .on \mousedown @onMouseDown
+    onMouseMove: !(e) ->
+      @setState do
+        x: @comp.x + e.pageX - @mouse.x
+        y: @comp.y + e.pageY - @mouse.y
+
+Hittable = do
+  list: []
+  status:
+    OUT:     0x00
+    OVERLAP: 0x01
+    IN:      0x03
+  mixin:
+    componentDidMount: !->
+      Hittable.list.push this
+    componentWillUnmount: !->
+      Hittable.list := Hittable.list.filter (isnt this), this
+    #componentWillUpdate: !(props, state) ->
+      # do the collision tests here
+      # changing the state will affect the new state
+
 CC ?= do
-  config:
-    width:  200
-    height: 200
   List: React.createClass do
     displayName: \List
+    mixins: [Draggable.mixin, Hittable.mixin]
     getInitialState: ->
       x: 200
       y: 200
-      width:  CC.config.width
-      height: CC.config.height
+      width:  config.width
+      height: config.height
       words: [0]
     leftClicked: (e) ->
       e.preventDefault!
       @state.words.unshift 0
       @setState do
-        x: @state.x - CC.config.width
-        width: @state.width + CC.config.width
+        x: @state.x - config.width
+        width: @state.width + config.width
         words: @state.words
       console.log @state.words
     rightClicked: (e) ->
       e.preventDefault!
       @state.words.push 0
       @setState do
-        width: @state.width + CC.config.width
+        width: @state.width + config.width
         words: @state.words
       console.log @state.words
     render: ->
@@ -35,20 +81,22 @@ CC ?= do
           top:    @state.y
           width:  @state.width
           height: @state.height
+        /*
         * a do
             className: 'hit left'
             style:
-              width:  CC.config.width / 2
-              height: CC.config.height
+              width:  config.width / 2
+              height: config.height
             href: '#'
-            onClick: @leftClicked
+            #onClick: @leftClicked
         * a do
             className: 'hit right'
             style:
-              width:  CC.config.width / 2
-              height: CC.config.height
+              width:  config.width / 2
+              height: config.height
             href: '#'
-            onClick: @rightClicked
+            #onClick: @rightClicked
+        */
 
-React.renderComponent CC.List!, document.getElementById \container
+React.renderComponent CC.List!, config.root
 
